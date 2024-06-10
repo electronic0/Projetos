aa1cf40fa5d354726ef9771d7c2de09cda603df2
electronic <78802199+electronic0@users.noreply.github.com>
2024-06-10 12:08:56 -0300

    Update file

--- Python/Ferramentas/Gerenciador de Senha.py
+++ Python/Ferramentas/Gerenciador de Senha.py
@@ -1,7 +1,7 @@
 from random import choice
 n = int(input('Insira o valor de senhas a ser gerado: '))
 q = int(input('Insira o valor da quantidade de caracteres a ser gerado: '))
-caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^*+=-?'
 for s in range(1, n + 1):
     senha = ''
     for c in range(1, q + 1):

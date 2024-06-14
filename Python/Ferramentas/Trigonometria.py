d072646980a2fcef294a2e65be19711dc517df5d
electronic <78802199+electronic0@users.noreply.github.com>
2024-06-14 15:06:33 -0300

    Create Trigonometria.py

---
+++ Python/Ferramentas/Trigonometria.py
@@ -0,0 +1,3 @@
+from math import radians, sin, cos ,tan
+a = float(input('Digite um angulo trigonométrico: '))
+print(f'O seno de {a}° é {sin(radians(a)):.4f} \nO cosseno de {a}° é {cos(radians(a)):.4f} \nE a tangente de {a}° é {tan(radians(a)):.4f}')
